e4c4127d30c8806a22b3bc56b3e20a73043df469
DonSimonetti <matt.simonetti@hotmail.it>
2017-05-02 17:31:50 +0200

    Compilation config

---
+++ cmake-build-release/CMakeFiles/sampPluginScLib.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/matts/CLionProjects/sampPluginScLib/main.cpp" "C:/Users/matts/CLionProjects/sampPluginScLib/cmake-build-release/CMakeFiles/sampPluginScLib.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
